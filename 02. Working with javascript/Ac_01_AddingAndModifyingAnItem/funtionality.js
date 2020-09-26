5eaabe2607c64e32d664a9070623fb9784c0ee60
Crhistian <matrixcristian21@gmail.com>
2020-09-26 00:37:02 -0500

    Ac 1 done

--- 02. Working with javascript/Ac_01_AddingAndModifyingAnItem/funtionality.js
+++ 02. Working with javascript/Ac_01_AddingAndModifyingAnItem/funtionality.js
@@ -1,1 +1,5 @@
-let 
+let taskList = document.getElementById('taskList');
+let newTask = document.createElement('li');
+newTask.innerText = 'walk the dog';
+taskList.appendChild(newTask);
+taskList.lastChild.style.color = 'red';
